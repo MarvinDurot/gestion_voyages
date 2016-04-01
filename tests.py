7af2435688cae1cf38cfc21f155ab89b7298af0a
MarvinDurot <marvin.dut@gmail.com>
2016-04-01 09:10:43 +0200

    Add docstrings

--- tests.py
+++ tests.py
@@ -1,43 +1,59 @@
-#
-# Test Unitaires
-# TODO : compléter les tests
-#
+"""
+Tests unitaires de l'application
+"""
+
 from models import City, User, Travel, Transport, Accomodation
 import unittest
 
+
 class City_TestCase(unittest.TestCase):
-    """Tests unitaires du modèle City
     """
+    Tests unitaires du modèle City.
+    """
+
     def test_HasDoc(self):
         self.assertLess(10, len(City.__doc__))
 
+
 class User_TestCase(unittest.TestCase):
-    """Tests unitaires du modèle User
     """
+    Tests unitaires du modèle User.
+    """
+
     def test_HasDoc(self):
         self.assertLess(10, len(User.__doc__))
 
+
 class Travel_TestCase(unittest.TestCase):
-    """Tests unitaires du modèle Travel
     """
+    Tests unitaires du modèle Travel.
+    """
+
     def test_HasDoc(self):
         self.assertLess(10, len(Travel.__doc__))
 
+
 class Transport_TestCase(unittest.TestCase):
-    """Tests unitaires du modèle Transport
     """
+    Tests unitaires du modèle Transport.
+    """
+
     def test_HasDoc(self):
         self.assertLess(10, len(Transport.__doc__))
 
+
 class Accomodation_TestCase(unittest.TestCase):
-    """Tests unitaires du modèle Accomodation
     """
+    Tests unitaires du modèle Accomodation.
+    """
+
     def test_HasDoc(self):
         self.assertLess(10, len(Accomodation.__doc__))
 
+
 if __name__ == '__main__':
     print("======Tests Start")
-    unittest.main(exit=False,verbosity=2)
-    a=input("fini : appuyez sur une touche!")
+    unittest.main(exit=False, verbosity=2)
+    a = input("fini : appuyez sur une touche!")
     print("=================")
-    print (__doc__)
+    print(__doc__)
