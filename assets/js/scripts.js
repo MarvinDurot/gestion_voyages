99997d225575909a82557892067cdc989e92cc51
MarvinDurot <marvin.dut@gmail.com>
2016-05-21 21:36:58 +0200

    Format date picker input value

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,8 +4,9 @@
 		$('.button-collapse').sideNav();
 		$('select').material_select();
 		$('.datepicker').pickadate({
-            selectMonths: true, // Creates a dropdown to control month
-            selectYears: 15 // Creates a dropdown of 15 years to control year
+            selectMonths: true,
+            selectYears: 15,
+            format: 'dd/mm/yyyy'
         });
 
 		$('.modal-trigger').leanModal();
