343fde35839ecd0f0cee55bae03b713ff1852a0e
MarvinDurot <marvin.dut@gmail.com>
2016-05-21 17:37:17 +0200

    Handle isCapital checkbox hiding

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,25 @@
 (function($){
-  $(function(){
+	$(function(){
 
-    $('.button-collapse').sideNav();
-    $('select').material_select();
-    $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
-  });
+		$('.button-collapse').sideNav();
+		$('select').material_select();
+		$('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
+        });
 
-  $('.modal-trigger').leanModal();
+		$('.modal-trigger').leanModal();
 
-  0
+		$('#is_capital').click(function() {
+		    if ($(this).is(':checked')) {
+		        $('#nearest_capital').hide();
+		        $('#capital_id').prop('selectedIndex', 0);
+		        $('#capital_id').removeAttr('required');
+		    } else {
+		        $('#nearest_capital').show();
+		        $('#capital_id').attr('required', 'true');
+		    }
+		});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
